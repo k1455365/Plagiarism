41e7be08a7b59a8f2c54a179c20c3095745aa837
k1455365 <tong.qu@kcl.ac.uk>
2015-08-17 23:59:23 +0100

    compare manager class

---
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/util/compareManager.java
@@ -0,0 +1,8 @@
+package kcl.qutong.plagiarism.util;
+
+public class compareManager {
+public String[] compareTool(String srcContent, String trgContent, int taskway){
+	String[] mixResult={"similarity","details","src content after process","trg content after process"};
+	return mixResult;
+}
+}
