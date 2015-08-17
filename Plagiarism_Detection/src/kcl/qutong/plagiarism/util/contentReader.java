55b1052d7dba4edc4d4ded5cc85a374d7952fc46
k1455365 <tong.qu@kcl.ac.uk>
2015-08-17 03:59:01 +0100

    modification for textfile and path as attribute of method

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/util/contentReader.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/util/contentReader.java
@@ -2,19 +2,20 @@ package kcl.qutong.plagiarism.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 /**
- * useing this class can read content with the type java support, this is project is mainly java file
+ * useing this class can read content with the type java support, this is
+ * project is mainly java file
+ * 
  * @author qutong
- *
+ * 
  */
 public class contentReader {
 	String content;
 	String Codecontent;
 	String Textcontent;
-
 
 	public String getContent() {
 		return content;
@@ -39,57 +40,70 @@ public class contentReader {
 	public void setTextcontent(String textcontent) {
 		Textcontent = textcontent;
 	}
+
 	/**
-	 * this method will select reader based on file type,this version type is selected by user
+	 * this method will select reader based on file type,this version type is
+	 * selected by user
+	 * 
 	 * @param filetype
 	 * @param file
 	 * @return
-	 * @throws IOException
+	 * @throws Exception
 	 */
-	public  String readerManage(int filetype,File file) throws IOException{
-//		1:text; 2:java
+	public String readerManage(int filetype, File file, String path)
+			throws Exception {
+		// 1:text; 2:java
 		switch (filetype) {
-		case 1:
-			content=buildin(file);
-			break;
 		case 2:
-			content=buildin(file);
+			content = buildin(file);
+			break;
+		case 1:
+			content = pdfType(path);
 			break;
 		}
 		return content;
 	}
+
 	/**
 	 * 
 	 * @param file
-	 * @return
-	 * content of file with string type data
+	 * @return content of file with string type data
 	 * @throws IOException
 	 */
-public String buildin(File file) throws IOException{
+	public String buildin(File file) throws IOException {
 
-	FileReader freader = new FileReader(file);
-    BufferedReader breader = new BufferedReader(freader);
-    StringBuilder sb = new StringBuilder();
-    try {
-        String temp = "";
-        /**
-         * 读取文件内容，并将读取的每一行后都不加\n
-         * 其目的是为了在解析双反斜杠（//）注释时做注释中止符
-         */
-        while((temp=breader.readLine())!= null)
-        {
-            sb.append(temp);
-            sb.append('\n');
-        }
-        Codecontent = sb.toString();
-    }catch (IOException e) {
-        System.out.println("cant read file");
-    }finally
-    {
-        breader.close();
-        freader.close();
-    }
-	
-	return Codecontent;
-}
+		FileReader freader = new FileReader(file);
+		BufferedReader breader = new BufferedReader(freader);
+		StringBuilder sb = new StringBuilder();
+		try {
+			String temp = "";
+			/**
+			 * 读取文件内容，并将读取的每一行后都不加\n 其目的是为了在解析双反斜杠（//）注释时做注释中止符
+			 */
+			while ((temp = breader.readLine()) != null) {
+				sb.append(temp);
+				sb.append('\n');
+			}
+			Codecontent = sb.toString();
+		} catch (IOException e) {
+			System.out.println("cant read file");
+		} finally {
+			breader.close();
+			freader.close();
+		}
+
+		return Codecontent;
+	}
+
+	public String pdfType(String path) throws Exception {
+		// String path =file.getAbsolutePath();
+		System.out.println("file path is: " + path);
+		TextFromPDF pdf = new TextFromPDF();
+		//Textcontent = pdf.TextFromPDF_new(path);
+		//Textcontent=pdf.pdftoText(path);
+		Textcontent=pdf.getTextFromPDF(path);
+		PDFReader pdfr=new PDFReader();
+		pdfr.readFdf(path);
+		return Textcontent;
+	}
 }
