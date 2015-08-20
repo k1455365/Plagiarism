57280dd75de0d76a5e75c80858acd29056c6636d
k1455365 <tong.qu@kcl.ac.uk>
2015-08-20 03:50:38 +0100

    juint test file and css file for **result.jsp

---
+++ Plagiarism_Detection/test/kcl/qutong/plagiarism/entity/SmithWaterman/BasicSmithWatermanTest.java
@@ -0,0 +1,25 @@
+package kcl.qutong.plagiarism.entity.SmithWaterman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicSmithWatermanTest {
+
+	@Test
+	public void test() {
+			String a1 = "a b c x d e f g h i y m z j l u k p q s j t u v";
+			String b = "a b c d e f g h i j k l m n o p q r s j t u v";
+			// String a1="abcxdefghiymzjlukpqsjtuv";
+			// String b="abcdefghijklmnopqrsjtuv";
+			// char[] seqA =a1.toCharArray();
+			// char[] seqB =b.toCharArray();
+			String[] seqA = a1.split(" ");
+			String[] seqB = b.split(" ");
+			BasicSmithWaterman aa = new BasicSmithWaterman();
+			aa.calScoreMatrix(seqA, seqB);
+			aa.traceback();
+
+	}
+
+}
