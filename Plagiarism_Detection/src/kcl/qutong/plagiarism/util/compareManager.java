b84cd7564630ee6a7b4cb0fe519535fdc582778e
k1455365 <tong.qu@kcl.ac.uk>
2015-08-21 23:25:32 +0100

    add manage about smith waterman with cutoff and without overlap

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/util/compareManager.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/util/compareManager.java
@@ -2,11 +2,13 @@ package kcl.qutong.plagiarism.util;
 
 import kcl.qutong.plagiarism.entity.SmithWaterman.BasicSmithWaterman;
 import kcl.qutong.plagiarism.entity.SmithWaterman.SmithWatermanArray;
+import kcl.qutong.plagiarism.entity.SmithWaterman.SmithWaterman_cutoff;
 
 public class compareManager {
 	String[] mixResult;
 	SmithWatermanArray swa = new SmithWatermanArray();
 	BasicSmithWaterman bsw = new BasicSmithWaterman();
+	SmithWaterman_cutoff swc=new SmithWaterman_cutoff();
 
 	public String[] compareTool(String srcContent, String trgContent,
 			int taskway, int algorithm) {
@@ -14,31 +16,93 @@ public class compareManager {
 		// mixResult={"similarity","details","src content after process","trg content after process"};
 		// String[]
 		// mixResult={"11","[1,3]&[3,6]#[4,9]&[6,8]","src content after process","trg content after process"};
-		switch (taskway) {
-		case 1: {
 
-			System.out
-					.println("enter text compare using basic smith waterman algorithm");
-			String[] srcArray =srcContent.split(" ");
-			String[] trgArray =trgContent.split(" ");
-			mixResult = bsw.result(srcArray, trgArray);
-			break;
+		switch (algorithm) {
+		case 1: {
+			switch (taskway) {
+			case 1: {
+
+				System.out
+						.println("enter text compare using basic smith waterman algorithm");
+				String[] srcArray = srcContent.split(" ");
+				String[] trgArray = trgContent.split(" ");
+				mixResult = bsw.result(srcArray, trgArray);
+				break;
+			}
+			case 2: {
+				System.out
+						.println("enter code compare using basic smith waterman algorithm");
+				String[] srcArray = srcContent.split("\n");
+				String[] trgArray = trgContent.split("\n");
+				mixResult = bsw.result(srcArray, trgArray);
+				break;
+			}
+			case 3: {
+				System.out
+						.println("enter token compare using basic smith waterman algorithm");
+				mixResult = swa.result(srcContent, trgContent);
+				break;
+			}
+			}
 		}
+//		smith waterman cutoff algorithm
 		case 2: {
-			System.out
-					.println("enter code compare using basic smith waterman algorithm");
-			String[] srcArray =srcContent.split("\n");
-			String[] trgArray =trgContent.split("\n");
-			mixResult = bsw.result(srcArray, trgArray);
-			break;
+			switch (taskway) {
+			case 1: {
+
+				System.out
+						.println("enter text compare using smith waterman cutoff algorithm");
+				String[] srcArray = srcContent.split(" ");
+				String[] trgArray = trgContent.split(" ");
+				mixResult = swc.result(srcArray, trgArray);
+				break;
+			}
+			case 2: {
+				System.out
+						.println("enter code compare using smith waterman cutoff algorithm");
+				String[] srcArray = srcContent.split("\n");
+				String[] trgArray = trgContent.split("\n");
+				mixResult = swc.result(srcArray, trgArray);
+				break;
+			}
+			case 3: {
+				System.out
+						.println("enter token compare using basic smith waterman algorithm");
+				mixResult = swa.result(srcContent, trgContent);
+				break;
+			}
+			}
 		}
 		case 3: {
-			System.out
-					.println("enter token compare using basic smith waterman algorithm");
-			mixResult = swa.result(srcContent, trgContent);
-			break;
+			switch (taskway) {
+			case 1: {
+
+				System.out
+						.println("enter text compare using smith waterman without overlap algorithm");
+				String[] srcArray = srcContent.split(" ");
+				String[] trgArray = trgContent.split(" ");
+				mixResult = bsw.result(srcArray, trgArray);
+				break;
+			}
+			case 2: {
+				System.out
+						.println("enter code compare using  smith waterman without overlap algorithm");
+				String[] srcArray = srcContent.split("\n");
+				String[] trgArray = trgContent.split("\n");
+				mixResult = bsw.result(srcArray, trgArray);
+				break;
+			}
+			case 3: {
+				System.out
+						.println("enter token compare using smith waterman without overlap algorithm");
+				mixResult = swa.result(srcContent, trgContent);
+				break;
+			}
+			}
 		}
 		}
+
+		
 		return mixResult;
 	}
 }
