3d34c3238e36de99f0e1bd90df9bd2eaedc805da
k1455365 <tong.qu@kcl.ac.uk>
2015-08-21 23:31:30 +0100

    java doc

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/action/logoutAction.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/action/logoutAction.java
@@ -1,7 +1,12 @@
 package kcl.qutong.plagiarism.action;
 
 import org.apache.struts2.ServletActionContext;
-
+/**
+ * can't stop logout simply jump to a new start page,
+ * it should stop the session
+ * @author qutong
+ *
+ */
 public class logoutAction {
 	public String execute() throws Exception
 	{
