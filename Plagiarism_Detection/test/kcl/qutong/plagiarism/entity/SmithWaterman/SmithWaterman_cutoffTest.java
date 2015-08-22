cfa21823cd7995713a33f9be3af2c9dfb5e6f551
k1455365 <tong.qu@kcl.ac.uk>
2015-08-22 23:58:08 +0100

    add test for smith waterman cut off

---
+++ Plagiarism_Detection/test/kcl/qutong/plagiarism/entity/SmithWaterman/SmithWaterman_cutoffTest.java
@@ -0,0 +1,14 @@
+package kcl.qutong.plagiarism.entity.SmithWaterman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SmithWaterman_cutoffTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
