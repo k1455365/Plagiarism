868700e2365fae438117a432f537f11ab21fa1be
k1455365 <tong.qu@kcl.ac.uk>
2015-08-23 00:22:23 +0100

    delete a useless portion

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/SmithWaterman/SmithWaterman_cutoff.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/SmithWaterman/SmithWaterman_cutoff.java
@@ -1,10 +1,127 @@
 package kcl.qutong.plagiarism.entity.SmithWaterman;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * this is revise version of smith waterman algorithm
+ * 
  * @author qutong
- *
+ * 
  */
 public class SmithWaterman_cutoff extends BasicSmithWaterman {
-int [][] M;// largest value of state in the traceback except the state itself
+	private int score; // represent the "goodness of fit"
+	private int m;// length of string X
+	private int n;// length of string Y
+	private int h;// hits
+	private int d;// delete of insert
+	private int r;// replace
+	String[] X;
+	String[] Y;
+	List<String> preX = new ArrayList<String>();
+	List<String> preY = new ArrayList<String>();
+	int[][] s;// score matrix
+	int[][] M;// matrix store largest value value before this point except itself.
+	point max = new point();
+	point origil = new point();
+	cell backPoint = new cell();
+	List<cell> path = new ArrayList<cell>();
+	cell[] backTrace;
+
+	public String[] result(String[] srcArray, String[] trgArray) {
+		System.out.println("enter basic smithwaterman");
+		String[] mix = new String[4];
+		calScoreMatrix(srcArray, trgArray);
+		traceback();
+		mix[0] = String.valueOf(max.getValue());
+		mix[1] = "[" + origil.getX() + "," + max.getX() + "]&[" + origil.getY()
+				+ "," + max.getY() + "]#";
+		mix[2] = "src";
+		mix[3] = "trg";
+		return mix;
+	}
+
+	public void calScoreMatrix(String[] srcArray, String[] trgArray) {
+		// public void calScoreMatrix(char[] srcArray, char[] trgArray) {
+		m = srcArray.length;
+		n = trgArray.length;
+		preX.add("0");
+		preY.add("0");
+		h = d = r = 1;
+		max.setValue(0);
+
+		// make the first column is zero
+		s = new int[m + 1][n + 1];
+		M= new int[m + 1][n + 1];
+		for (int i = 0; i < m; i++) {
+			preX.add(srcArray[i]);
+			s[i][0] = 0;
+			M[i][0] = 0;
+		}
+		// make the first row is zero
+		for (int j = 0; j < n; j++) {
+			preY.add(trgArray[j]);
+			s[0][j] = 0;
+			M[0][j] = 0;
+		}
+
+//		for (int i = 0; i < m + 1; i++) {
+//			s[i][0] = 0;
+//		}
+//		// make the first row is zero
+//		for (int j = 0; j < n + 1; j++) {
+//			s[0][j] = 0;
+//		}
+		X = preX.toArray(new String[1]);
+		Y = preY.toArray(new String[1]);
+		// start from 1 is because previous work hence the result could be
+		// negative
+		for (int i = 1; i <= m; i++) {
+			for (int j = 1; j <= n; j++) {
+				// System.out.println("i=" + i + " j=" + j);
+				if (X[i].equalsIgnoreCase(Y[j])) {
+					// if (X[i-1]==Y[j-1]) {
+					s[i][j] = s[i - 1][j - 1] + h;
+				} else {
+
+					s[i][j] = Math.max(
+							0,
+							Math.max(
+									s[i - 1][j] - d,
+									Math.max(s[i][j - 1] - d, s[i - 1][j - 1]
+											- r)));
+				}
+				// judge and store the max value and its position
+				if (max.getValue() <= s[i][j]) {
+					max.setValue(s[i][j]);
+					max.setX(i);
+					max.setY(j);
+				} else {
+
+				}
+			}
+		}
+	}
+
+	public void traceback() {
+		int i = max.getX();
+		int j = max.getY();
+		while (s[i][j] != 0) {
+			backPoint.setX(i);
+			backPoint.setY(j);
+			path.add(backPoint);
+			if (X[i].equalsIgnoreCase(Y[j])) {
+				// if (X[i]==Y[j]) {
+				i--;
+				j--;
+			} else if (s[i][j] == s[i][j - 1] - 1) {
+				j--;
+			} else {
+				i--;
+			}
+			origil.setX(i);
+			origil.setY(j);
+		}
+	}
 
 }
