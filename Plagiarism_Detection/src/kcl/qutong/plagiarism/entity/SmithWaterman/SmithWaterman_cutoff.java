e76645c7ab453bc794e659595b6f811f979595aa
k1455365 <tong.qu@kcl.ac.uk>
2015-08-22 23:56:59 +0100

    cut off

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/SmithWaterman/SmithWaterman_cutoff.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/SmithWaterman/SmithWaterman_cutoff.java
@@ -1,5 +1,9 @@
 package kcl.qutong.plagiarism.entity.SmithWaterman;
-
+/**
+ * this is revise version of smith waterman algorithm
+ * @author qutong
+ *
+ */
 public class SmithWaterman_cutoff extends BasicSmithWaterman {
 
 }
