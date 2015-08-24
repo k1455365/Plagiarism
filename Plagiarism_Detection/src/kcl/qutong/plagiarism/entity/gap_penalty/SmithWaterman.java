70d9f2f714b6677920c7afde558680ef13db693d
k1455365 <tong.qu@kcl.ac.uk>
2015-08-24 20:52:41 +0100

    smith waterman gotho version

--- Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/gap_penalty/SmithWaterman.java
+++ Plagiarism_Detection/src/kcl/qutong/plagiarism/entity/gap_penalty/SmithWaterman.java
@@ -168,15 +168,5 @@ public class SmithWaterman {
 		System.out.println();
 	}
 
-	public static void main(String[] args) {
-		// DNA sequence Test:
-//		SmithWaterman sw = new SmithWaterman("ACAC{ACTA", "AGCACA;CA");
-//		System.out.println("Alignment Score: " + sw.computeSmithWaterman());
-//		sw.printMatrix();
 
-		SmithWaterman sw = new SmithWaterman("Then Jesus answering said unto them, Go your way, and tell John what things ye have seen and heard; how that the blind see, the lame walk, the lepers are cleansed, the deaf hear, the dead are raised, to the poor the gospel is preached.", "The blind receive their sight, and the lame walk, the lepers are cleansed, and the deaf hear, the dead are raised up, and the poor have the gospel preached to them.");
-//		SmithWaterman sw = new SmithWaterman("But what went ye out for to see? A man clothed in soft raiment? Behold, they which are gorgeously apparelled, and live delicately, are in kings' courts.","But what went ye out for to see? A man clothed in soft raiment? behold, they that wear soft clothing are in kings' houses.");
-		System.out.println("Alignment Score: " + sw.computeSmithWaterman());
-		//sw.printMatrix();
-	}
 }
